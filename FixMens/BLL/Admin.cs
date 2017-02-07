5388a684ffb7486fc74dc501f9e20f63d0972122
Martin <martin.nv6@gmail.com>
2017-02-07 11:55:46 -0600

    Se limpio comentarios en Admin.cs y se realizo query para que regrese solo un valor

--- FixMens/BLL/Admin.cs
+++ FixMens/BLL/Admin.cs
@@ -14,26 +14,12 @@ namespace FixMens.BLL
         {
             FbConnection conn = new FbConnection();
             FbCommand cmd = new FbCommand();
-            FbDataAdapter da = new FbDataAdapter();
-            DataTable dt = new DataTable();
-            string firebirdServer = ConfigurationManager.AppSettings["firebird.server"];
-            //conn.ConnectionString = "User=SYSDBA;Password=masterkey;Database=localhost:C:\\service.fdb;DataSource=localhost;" +
-            //"Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=15;Pooling=true;" +
-            //"MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";.
-            conn.ConnectionString = "User=SYSDBA;Password=masterkey;Database=" + firebirdServer +
-               ";Port=3050;Dialect=3;Charset=NONE;Role=;Connection lifetime=15;Pooling=true;" +
-               "MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["firebirdConnection"].ToString();
             cmd.Connection = conn;
-            //int? cod = (orden);
-            //FbParameter pMatricula = new FbParameter();
-            
             cmd.CommandText = "EXECUTE PROCEDURE CONTAR_REPARACIONES 1";
             cmd.CommandType = CommandType.StoredProcedure;
-
-
             conn.Open();
-            da.SelectCommand = cmd;
-            da.Fill(dt);
+            int id = Convert.ToInt32(cmd.ExecuteScalar());
             conn.Close();
         }
     }
